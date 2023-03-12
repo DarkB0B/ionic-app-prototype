8687273dabadea050fcf6e4e7ddc58351fd5808a
Michal-Dudzik <dumichal20@wp.pl>
2023-03-12 20:05:44 +0100

    nicer add party button

--- src/app/view-newparty/view-newparty.page.ts
+++ src/app/view-newparty/view-newparty.page.ts
@@ -7,21 +7,25 @@ import { DataService, Party } from '../services/data.service';
   styleUrls: ['./view-newparty.page.scss'],
 })
 export class ViewNewpartyPage {
-  nameValue:string = '';
-  descriptionValue:string = '';
+  nameValue: string = '';
+  descriptionValue: string = '';
   dateValue = '';
   data: DataService;
 
-
   constructor(data: DataService) {
-    this.data = data
+    this.data = data;
   }
-
 
   Save() {
     let ids = this.data.parties.length;
 
-    this.data.addParty({name:this.nameValue, description:this.descriptionValue, date:this.dateValue.split('T')[0], users: [this.data.users[0], this.data.users[1]], id:ids});
-    console.log(this.data.parties[3])
+    this.data.addParty({
+      name: this.nameValue,
+      description: this.descriptionValue,
+      date: this.dateValue.split('T')[0],
+      users: [this.data.users[0], this.data.users[1]],
+      id: ids,
+    });
+    console.log(this.data.parties[3]);
   }
 }
